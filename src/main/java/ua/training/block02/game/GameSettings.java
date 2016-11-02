10688d98a3936302d9dae0875461d3dc4877af4a
AlexanderBogomolnyy <alexander.bogomolnyy@gmail.com>
2016-11-02 20:37:05 +0200

    Block 02 - Home Task - More-Less game ver. 1.01 Made some code refactoring.

---
+++ src/main/java/ua/training/block02/game/GameSettings.java
@@ -0,0 +1,18 @@
+package ua.training.block02.game;
+
+/**
+ * <p> The interface describing game settings.
+ *
+ * @author Alexander Bogomolnyy
+ * @version 1.0 30.10.2016.
+ */
+public interface GameSettings {
+    /**
+     * A constant holding the minimum primary barrier.
+     */
+    int PRIMARY_MIN_BARRIER = 0;
+    /**
+     * A constant holding the maximum primary barrier.
+     */
+    int PRIMARY_MAX_BARRIER = 100;
+}
